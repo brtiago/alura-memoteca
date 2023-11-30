2a96bd4dcbf42b87758a2050f420628eb62d7491
brtiago <tgribeiro@gmail.com>
2023-11-30 15:13:56 -0300

    feat: adding Rodape component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, CabecalhoComponent]
+    imports: [CommonModule, RouterOutlet, CabecalhoComponent, RodapeComponent]
 })
 export class AppComponent {
   title = 'memoteca';
