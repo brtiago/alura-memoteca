433bd6c707732db7f154b498a1eaa8fbec0c9685
brtiago <tgribeiro@gmail.com>
2023-12-04 11:40:55 -0300

    feat: add pensamento component

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class CriarPensamentoComponent {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
 }
