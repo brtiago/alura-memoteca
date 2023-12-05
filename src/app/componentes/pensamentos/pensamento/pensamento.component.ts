8d5b37c25aa03ceed329a7ac7a3d5a1bae8e4ae6
brtiago <tgribeiro@gmail.com>
2023-12-05 11:19:28 -0300

    feat: add pensamento component

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.scss'
+})
+export class PensamentoComponent {
+
+}
