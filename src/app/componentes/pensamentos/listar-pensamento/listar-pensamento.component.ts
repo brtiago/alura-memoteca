10cc538faba4d75711315b2a0f708146e9e10721
brtiago <tgribeiro@gmail.com>
2023-12-08 13:18:10 -0300

    feat: adding larguraPensamento component

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,7 +17,13 @@ export class ListarPensamentoComponent {
       conteudo: 'I love java',
       autoria: 'Tiago',
       modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a tortor et dui pellentesque feugiat. Proin placerat nisi ac laoreet mollis. Donec in eleifend neque. Phasellus scelerisque est auctor convallis faucibus. Quisque pharetra mauris neque. Nullam felis mi, hendrerit quis quam ut, pretium semper nulla. Sed hendrerit, orci eget imperdiet condimentum, dui ligula lacinia ligula, in volutpat nisl nunc sed neque. Sed velit felis, tempus vel accumsan eget, consectetur quis arcu. Sed vel ullamcorper nisi. Mauris vitae nisi rhoncus, pharetra leo quis, volutpat massa. ',
+      autoria: 'Tiago',
+      modelo: 'modelo1'
     }
+
   ];
 
 }
