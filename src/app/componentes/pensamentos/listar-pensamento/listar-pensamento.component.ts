b48a1eab1beb5d187d9d4b07e27ecd0007fe80d6
brtiago <tgribeiro@gmail.com>
2023-12-08 13:01:05 -0300

    feat: adding a list os components

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
+  listaPensamentos = [
+    {
+      conteudo: 'I love angular',
+      autoria: 'Tiago',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'I love java',
+      autoria: 'Tiago',
+      modelo: 'modelo2'
+    }
+  ];
+
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent {
 
-  pensamento = {
+  @Input() pensamento = {
     conteudo: 'I love angular',
     autoria: 'Tiago',
     modelo: 'modelo3'
